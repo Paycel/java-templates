d227747322c2c4add5cce730bd27bb859e983636
Nikita <huskyray@mail.ru>
2021-02-20 19:49:21 +0300

    practice 9 - gradle

---
+++ practice09-gradle/src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    /*
+    Создать приложение, которое выводит какое-то сообщение в консоль.
+    Создать Gradle Task, который создает jar-файл приложения, переносит его в отдельную папку, в которой
+    хранится Dockerfile для jar, а затем создает Docker контейнер из данного jar-файла и запускает его.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi, it's gradle");
+    }
+}

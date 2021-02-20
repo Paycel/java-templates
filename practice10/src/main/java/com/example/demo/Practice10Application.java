e3b84fc7e8664b3a6b955b4f41c20aea41498cba
Nikita <huskyray@mail.ru>
2021-02-20 21:44:11 +0300

    added variant

--- practice10/src/main/java/com/example/demo/Practice10Application.java
+++ practice10/src/main/java/com/example/demo/Practice10Application.java
@@ -11,6 +11,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @SpringBootApplication
 public class Practice10Application {
 
+    /*
+    Создать приложение, в котором создается ApplicationContext и из него берётся бин с названием,
+    переданным в качестве аргумента к приложению, и вызывается метод интерфейса, который он имплементирует.
+    Нужно создать по одному бину для каждого класса, определить им название.
+    Проверить, что вызывается при вводе названия каждого из бинов.
+    Классы и интерфейс определяются в соответствии с вариантом индивидуального задания.
+
+    Интерфейс Musician с методом doCoding(), его имплементации: Drummer, guitarist, trombonist.
+     */
+
     public static void main(String[] args) {
         SpringApplication.run(Practice10Application.class, args);
         ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
